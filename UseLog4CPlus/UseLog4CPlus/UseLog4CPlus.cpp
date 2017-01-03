2a9c9528f426ed963498cf84cdbae789059bc1da
Ryan Zhang <dameng.zhang@carestream.com>
2017-01-03 14:55:56 +0800

    Example about how to use log4cplus

---
+++ UseLog4CPlus/UseLog4CPlus/UseLog4CPlus.cpp
@@ -0,0 +1,30 @@
+// UseLog4CPlus.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "log4cplus/logger.h"
+#include "log4cplus/loggingmacros.h"
+#include "log4cplus/configurator.h"
+#include "log4cplus/initializer.h"
+
+#ifdef _DEBUG
+	#pragma comment(lib, "log4cplus/log4cplusD.lib")
+#else
+	#pragma comment(lib, "log4cplus/log4cplus.lib")
+#endif // _DEBUG
+
+
+int main()
+{
+	log4cplus::Initializer initializer;
+	log4cplus::PropertyConfigurator::doConfigure(_T("log4cplus.ini"));	
+	/*log4cplus::BasicConfigurator config;
+	config.configure();*/
+
+	log4cplus::Logger logger = log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("Main"));
+	LOG4CPLUS_INFO_STR(logger, _T("this is the first string."));
+	LOG4CPLUS_INFO_FMT(logger, _T("this is the second string %s."), _T("with format"));
+	LOG4CPLUS_INFO(logger, _T("output the current thread:") << 2);
+	return 0;
+}
+
